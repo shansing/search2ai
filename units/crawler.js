d428cdbb4b53a065d67baf3316b0f194b4dd7a7d
Shansing <i@shansing.com>
2024-06-20 20:58:08 +0800

    feat: 禁止爬取本地网络

--- units/crawler.js
+++ units/crawler.js
@@ -4,11 +4,22 @@ const Readability = require('@mozilla/readability');
 const NodeHtmlMarkdown = require('node-html-markdown');
 const process = require("process");
 const {AbortError} = require("node-fetch");
+const dns = require('dns');
+const net = require('net');
 
 async function crawler(url) {
     console.log(`crawler:`, url);
     if (url && !url.startsWith('http')) {
         url = "http://" + url
+    }
+    let urlAllowed
+    try {
+        urlAllowed = await isUrlAllowed(url);
+    } catch (error) {
+        throw Error('Hostname cannot be resolved; ' + error.message)
+    }
+    if (!urlAllowed) {
+        throw Error('Intranet Addresses Forbidden!')
     }
     const controller = new AbortController();
     const timeout = setTimeout(() => {
@@ -124,5 +135,59 @@ async function useNinjaAi(url) {
         clearTimeout(timeout);
     }
 }
+
+function isLocalOrIntranetAddress(ip) {
+    if (net.isIPv4(ip)) {
+        const ipNum = ip.split('.').reduce((acc, cur) => (acc << 8) + parseInt(cur, 10), 0) >>> 0;
+        const privateNetworks = [
+            [0x0A000000, 0x0AFFFFFF], // 10.0.0.0/8
+            [0x7F000000, 0x7FFFFFFF], // 127.0.0.0/8
+            [0xAC100000, 0xAC1FFFFF], // 172.16.0.0/12
+            [0xC0A80000, 0xC0A8FFFF]  // 192.168.0.0/16
+        ];
+        for (const [start, end] of privateNetworks) {
+            if (ipNum >= start && ipNum <= end) {
+                return true;
+            }
+        }
+    }
+
+    if (net.isIPv6(ip)) {
+        // 检查 ::1 地址
+        if (ip === '::1') {
+            return true;
+        }
+
+        const ipHex = ip.replace(/:/g, '');
+        const firstByte = parseInt(ipHex.substr(0, 2), 16);
+        return (
+            (firstByte >= 0x0A && firstByte <= 0x0A) || // fe80::/10
+            (firstByte >= 0xFD && firstByte <= 0xFE)   // fc00::/7
+        );
+    }
+
+    return false;
+}
+
+function isUrlAllowed(url) {
+    try {
+        const parsedUrl = new URL(url);
+        const hostname = parsedUrl.hostname;
+
+        return new Promise((resolve, reject) => {
+            dns.resolve(hostname, (err, addresses) => {
+                if (err) {
+                    return reject(err);
+                }
+
+                const isAllowed = addresses.every(address => !isLocalOrIntranetAddress(address));
+                resolve(isAllowed);
+            });
+        });
+    } catch (error) {
+        return Promise.reject(error);
+    }
+}
+
 
 module.exports = crawler;
